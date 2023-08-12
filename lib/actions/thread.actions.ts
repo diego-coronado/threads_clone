3badce874d5fbee4276351110493dc37709489c3
Diego Coronado <dcoronado@able.co>
2023-08-12 17:10:56 +0900

    add home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -52,3 +52,39 @@ export async function createThread({
     throw new Error(`Failed to create thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+    // Calculatethe number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error) {
+    console.log(error)
+  }
+}
