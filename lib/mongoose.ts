002dfa7c8a7835deb7354376dd3082b8f0c21b6e
Diego Coronado <dcoronado@able.co>
2023-08-12 16:46:28 +0900

    add thread basic actions and create thread components

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-export const conectToDB = async () => {
+export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
