623745f97da443323b2479a572408c5aaeeb1274
Diego Coronado <dcoronado@able.co>
2023-08-13 17:55:16 +0900

    fix layout and onboarding page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
+import { dark } from "@clerk/themes";
 
-import "../globals.css"
+import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Threads",
@@ -17,9 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
-        <body className={`${inter.className} bd-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+          {children}
+          </div>
+          </body>
       </html>
     </ClerkProvider>
   );
